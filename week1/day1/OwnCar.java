dfb0ddad26c82bc3a51235b2f2a777a7c5f966c9
br877 <85825274+br877@users.noreply.github.com>
2021-07-24 18:02:34 +0530

    Add files via upload

---
+++ week1/day1/OwnCar.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class OwnCar {
+		
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		myCar.driveCar();
+		myCar.applyBrake();
+		
+		
+		
+	
+	}
+
+}
